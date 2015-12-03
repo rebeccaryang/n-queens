edcd21b8e5470410a4fbb80c27894e6c7629ddd8
rebeccaryang <rebeccayang123@gmail.com>
2015-12-02 21:03:27 -0800

    Starts queen problem

--- src/solvers.js
+++ src/solvers.js
@@ -50,17 +50,66 @@ window.findNQueensSolution = function(n) {
   if(n <= 3){
     return 0;
   }
+  //create helper function that takes a (board, [i,j])
+  // recursively call helper function while incrementing i and j 
+  var rounds = n;
   var queensBoard = new Board({n:n});
-  queensBoard.togglePiece(0,1);
-  for(var i = 1; i<n;i++){
-    for(var j = 0;j<n;j++){
-      queensBoard.togglePiece(i,j);
-      if(queensBoard.hasAnyQueensConflicts()){
-        queensBoard.togglePiece(i,j);
-      }
+  var solution = [];
+  var helperFunc = function(board, coordinate){
+    
     }
   }
-  var solution = queensBoard.rows();
+
+generateSuccessQueens(rounds);
+}
+  // var SolutionTree = function(value){
+
+  //   var obj = Object.create(methods);
+  //   obj.value = value;
+  //   obj.children = [];
+  //   return obj;
+  // }
+
+  // var methods = {};
+  // methods.addChild = function(value){
+  //   var child = SolutionTree(value)
+  //   this.children.push(child);
+  // }
+  // var rowsAdded = 0;
+  // // var traverse = function(){
+  // //   if(rowsAdded === n-1){
+  // //     return;
+  // //   }
+  // //   var queensTree = SolutionTree(null);
+  // //   for(var i = rowsAdded; i < n; i++){
+  // //     if(hasAnyQueensConflictsOn(rowsAdded, i){
+  // //       return;
+  // //     }
+  // //     queensTree[i].addChild([0,i]);
+
+  // //   }
+  // //   rowsAdded++;
+  // //   queensTree
+  // methods.traverse = function(value){
+  //   var queensTree = SolutionTree(null);
+    
+
+  }
+
+
+
+
+
+  // queensBoard.togglePiece(0,1);
+  // for(var i = 1; i<n;i++){
+  //   for(var j = 0;j<n;j++){
+  //     queensBoard.togglePiece(i,j);
+  //     if(queensBoard.hasAnyQueensConflicts()){
+  //       queensBoard.togglePiece(i,j);
+  //     }
+  //   }
+  // }
+  // var solution = queensBoard.rows();
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
